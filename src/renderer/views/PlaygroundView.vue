c7b3975b9f3f3d4c9d9ff66d0d3662f31e1441ed
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-04-17 19:44:18 +0200

    feat: implement resizable components and enhance settings navigation

--- src/renderer/views/PlaygroundView.vue
+++ src/renderer/views/PlaygroundView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-
+import ResizableDiv from "@/components/ResizableDiv";
 </script>
 
 <template>
-  <div class="text-12px p-2 w-full ">
-    <header class="italic opacity-50 text-primary-1000">
-      This is the playground area, use this view to test out and develop components
-    </header>
+  <div class="w-full h-full flex">
+    <ResizableDiv
+      name="playground"
+      side="right"
+      class="min-h-8 min-w-8 max-w-80vw"
+      default-size="80em"
+    >
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
+      inventore repellendus, ratione libero commodi tempore cum repudiandae
+      ipsam eligendi adipisci velit delectus pariatur? Modi ratione dolore
+      voluptatem vitae tempora quis?
+    </ResizableDiv>
   </div>
 </template>
